385dca74a4ba6404d8603f4cb463411cfa4dfbee
sks006 <mdshihabkabir07@gmail.com>
2025-12-03 08:33:57 +0600

    30 resutt prectice done

---
+++ 3_12_2025/option_result/src/main.rs
@@ -0,0 +1,472 @@
+
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("Cannot divide by zero".to_string())
+
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+
+// fn main(){
+//     let result =divide(10.0, 3.0);
+//     match result{
+//         Ok(value) =>println!("Result is {}", value),
+//         Err(err) =>println!("Error: {}", err),
+//     }
+// }
+
+
+//------------------2--------------------
+
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b== 0.0{
+//         Err("Cannot divide by zero".to_string())}else{
+//             Ok(a/b)
+//         }
+// }
+
+// fn main(){
+//     let result=divide(10.0, 0.0);
+//     match result{
+//         Err(err)=>println!("Error: {}", err),
+//         Ok(value)=>println!("Result is {}",value)
+//     }
+// }
+
+//------------------3--------------------
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("cannot divide by zero".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+
+// fn main(){
+//     let result=divide(10.0, 4.0);
+//     match result{
+//         Ok(value)=>println!("Result is {}",value),
+//         Err(err)=>println!("Error: {}", err),
+//     }
+// }
+//------------------4--------------------
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("cannot divide by zero".to_string())
+//     }else{
+//         Ok(a/b)}
+// }
+
+// fn main(){
+//     let result=divide(10.3,2.3);
+//     match result{
+//         Err(err)=>println!("Error: {}", err),
+//         Ok(value)=>println!("Result is {}",value)
+//     }
+// }
+//------------------5--------------------
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("cannot divide by zero".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+// fn main(){
+//     let result=divide(15.0,0.0);
+//     match result{
+//         Ok(value)=>println!("Result is {}",value),
+//         Err(err)=>println!("Error: {}", err),
+//     }
+// }
+//------------------6--------------------
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("cannot divide by zero".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+// fn main(){
+//     let result=divide(20.3,3.4);
+//     match result{
+//         Err(err)=>println!("Error: {}", err),
+//         Ok(value)=>println!("Result is {}",value)
+//     }
+// }
+//------------------7--------------------
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("cannot divide by zero".to_string())
+    
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+// fn main(){
+//     let result= divide(32.2,3.3);
+//     match result{
+//         Ok(value)=>println!("Result is {}",value),
+//         Err(err)=>println!("Error: {}", err),
+//     }
+// }
+
+//------------------8--------------------
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("cannot divide by zero".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+// fn main(){
+//     let result=divide(45.0,2.0);
+//     match result{
+//         Err(err)=>println!("ERROR: {}", err),
+//         Ok(value)=>println!("Result is {}",value)
+//     }
+// }
+//------------------9--------------------
+
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("Cannot divide by zero".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+// fn main(){
+//     let result=divide(50.0,0.0);
+//     match result{
+//         Ok(value)=>println!("Result is {}",value),
+//         Err(err)=>println!("Error: {}", err),
+//     }
+// }
+//------------------10--------------------
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+// if b==0.0{
+//     Err("Cannot divide by zero".to_string())
+// }else{
+//     Ok(a/b)
+// }
+// }
+// fn main(){
+//     let result=divide(60.0,4.0);
+//     match result{
+//         Err(err)=>println!("Error: {}", err),
+//         Ok(value)=>println!("Result is {}",value)
+//     }
+// }
+//------------------11--------------------
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("Cannot divide by zero".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+
+// fn main(){
+//     let result =divide(59.0,4.0);
+//     match result{
+//         Ok(value)=>println!("Result is {}",value),
+//         Err(err)=>println!("Error: {}", err),
+//     }
+// }
+
+//------------------12--------------------
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("Cannot divide by zero".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+// fn main(){
+//     let result=divide(200.0,3.2);
+//     match result{
+//         Err(err)=>println!("Error: {}", err),
+//         Ok(value)=>println!("Result is {}",value)
+//     }
+// }
+//------------------13--------------------
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("Cannot divide by zero".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+// fn main(){
+//     let result=divide(100.0,0.0);
+//     match result{
+//         Ok(value)=>println!("Result is  {}",value),
+//         Err(err)=>println!("Error: {}", err),
+//     }
+// }
+
+//------------------14--------------------
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("Cannot divide by zero".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+// fn main(){
+//     let result= divide(73.0,23.0);
+//     match result{
+//         Err(err)=>println!("Error:{}", err),
+//         Ok(value)=>println!("Result is {}",value)
+//     }
+// }
+//------------------15--------------------
+
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("cannot divide by zero".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+
+
+// fn main(){
+//     let result=divide(81.0,9.0);
+//     match result{
+//         Ok(value)=>println!("Result is {}",value),
+//         Err(err)=>println!("Error: {}", err),
+//     }
+// }
+
+//------------------16--------------------
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("cannot divide by zero".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+// fn main(){
+//     let result=divide(90.0,4.0);
+//     match result{
+//         Err(err)=>println!("Error: {}", err),
+//         Ok(value)=>println!("Result is {}",value),
+//     }
+// }
+//------------------17--------------------
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("cannot divide by zero".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+// fn main(){
+//     let result=divide(120.4,3.2);
+//     match result{
+//         Ok(value)=>println!("Result is {}",value),
+//         Err(err)=>println!("Error: {}", err),
+//     }
+// }
+//------------------18--------------------
+
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("cannot divide by zero".to_string())
+//     }else{
+//         Ok(a/b)}
+// }
+// fn main(){
+//     let result= divide(34.3,24.3);
+//     match result{
+//         Err(err)=>println!("Error: {}", err),
+//         Ok(value)=>println!("Result is {}",value)
+//     }
+// }
+//------------------19--------------------
+// fn divide(a:u32,b:u32)->Result<u32,String>{
+//     if b==0{
+//         Err("cannot divide by zero".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+// fn main(){
+//     let result=divide(100,0);
+//     match result{
+//         Ok(value)=>println!("Result is {}",value),
+//         Err(err)=>println!("Error: {}", err),
+//     }
+// }
+//------------------20--------------------
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("cannot divide by zero".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+// fn main(){
+//     let result=divide(150.0,32.0);
+//     match result{
+//         Err(err)=>println!("Error: {}", err),
+//         Ok(value)=>println!("Result is {}",value)
+// }}
+
+//------------------21--------------------
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("cannot divide by zero".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+// fn main(){
+//     let result=divide(75.9,3.3);
+//     match result{
+//         Ok(value)=>println!("Result is {}",value),
+//         Err(err)=>println!("Error: {}", err),
+//     }
+// }
+//------------------22--------------------
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("cannot divide by zero".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+// fn main(){
+//     let result=divide(88.6,4.5);
+//     match result{
+//         Err(err)=>println!("Error:{}", err),
+//         Ok(value)=>println!("Result is {}",value)
+//     }
+// }
+
+//------------------23--------------------
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("cannot divide by zero".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+// fn main(){
+//     let result=divide(99.9,0.0);
+//     match result{
+//         Ok(value)=>println!("Result is {}",value),
+//         Err(err)=>println!("Error: {}", err),
+//     }
+// }
+//------------------24--------------------
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("cannot divide by zero".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+
+// }
+// fn main(){
+//     let result=divide(1244.3,23.4);
+//     match result{
+//         Err(err)=>println!("Error: {}",err),
+//         Ok(value)=>println!("Result is {}",value)
+//     }
+// }
+
+//------------------25--------------------
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("cannot divide by zero".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+// fn main(){
+//     let result= divide(500.0,25.0);
+//     match result{
+//         Ok(value)=>println!("Result is {}",value),
+//         Err(err)=>println!("Error: {}", err),
+//     }
+// }
+
+//------------------26--------------------
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+// if b==0.0{
+//     Err("cannot divide by zero".to_string())
+// }else{
+//     Ok(a/b)
+// }
+// }
+
+// fn main(){
+//     let  result=divide(100.0,30.0);
+//     match result{
+//         Err(err)=>println!("Error: {}", err),
+//         Ok(value)=>println!("Result is {}",value),
+//     }
+// }
+//------------------27--------------------
+//  fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("cannot divide by zero".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+// fn main(){
+//     let result =divide(77.7,40.0);
+//     match result{
+//         Ok(value)=>println!("Result is {}",value),
+//         Err(err)=>println!("Error: {}", err),
+//     }
+// }
+//------------------28--------------------
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("cannot divide by zero".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+// fn main(){
+//     let result=divide(66.6,0.0);
+//     match result{
+//         Err(err)=>println!("Error: {}", err),
+//         Ok(value)=>println!("Result is {}",value),
+// }
+// }
+//------------------29--------------------
+// fn divide(a:f32,b:f32)->Result<f32,String>{
+//     if b==0.0{
+//         Err("cannot divide by zero".to_string())
+//     }else{
+//         Ok(a/b)
+//     }
+// }
+// fn main(){
+//     let result=divide(123.3,3.3);
+//     match result {
+//         Ok(value)=>println!("Result is {}",value),
+//         Err(err)=>println!("Error: {}", err),
+//     }
+// }
+//------------------30--------------------
+fn divide(a:f32,b:f32)->Result<f32,String>{
+    if b==0.0{
+        Err("cannot divide by zero".to_string())
+    }else{
+        Ok(a/b)
+    }
+}
+fn main(){
+    let result=divide(144.0,12.0);
+    match result{
+        Err(err)=>println!("Error: {}", err),
+        Ok(value)=>println!("Result is {}",value),
+    }
+}
