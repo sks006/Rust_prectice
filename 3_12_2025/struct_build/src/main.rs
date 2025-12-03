d7c25049487e305c11690a189357b64449fbec12
sks006 <mdshihabkabir07@gmail.com>
2025-12-03 19:33:01 +0600

    struct

--- 3_12_2025/struct_build/src/main.rs
+++ 3_12_2025/struct_build/src/main.rs
@@ -268,9 +268,90 @@
 
 //--------------------- 11 --------------------------
 
+// enum Status{
+//     Pass,
+//     Reje
+// }
+// struct Loan{
+//     acc:u32,
+//     status:Status
+// }
+// impl Loan{
+//     fn new(a:u32,s:Status)->Self{
+//         Self{
+//             acc:a,
+//             status:s
+//         }
+//     }
+// }
+// fn main(){
+//     let l=Loan::new(30,Status::Pass);
+//     match l.status{
+//         Status::Pass=>println!("pass {}",l.acc),
+//         Status::Reje=>println!("reje {}",l.acc)
+//     }
+// }
+
+// ----------------- 12 ----------------
+
+// enum Status{
+//     Pass,
+//     Reje
+// }
+// struct Loan{
+//     acc:u32,
+//     status:Status
+// }
+
+// impl Loan{
+//     fn new(a:u32,s:Status)->Self{
+//         Self{
+//             acc:a,
+//             status:s
+//         }
+//     }
+// }
+// fn main(){
+//     let l=Loan::new(32,Status::Pass);
+//     match l.status{
+//         Status::Pass=>println!("pass {}",l.acc),
+//         Status::Reje=>println!("reje {}",l.acc),
+//     }
+
+// }
+
+// ---------------------- 13 ----------------------
+
+// enum Status{
+//     Pass,
+//     reje
+// }
+
+// struct Loan{
+//     acc:u32,
+//     status:Status,
+// }
+// impl Loan{
+//     fn new(a:u32,s:Status)->Self{
+//         Self{
+//             acc:a,
+//             status:s
+//         }
+//     }
+// }
+
+// fn main(){
+//     let l=Loan::new(44,Status::Pass);
+//     match l.status{
+//         Status::Pass=>println!("pass {}",l.acc),
+//         Status::reje=>println!("Reje {}",l.acc)
+//     }
+// }
+
+// ---------------------- 14 ----------------------
 enum Status{
     Pass,
-    Reje
+    reje
 }
 struct Loan{
     acc:u32,
@@ -285,10 +366,11 @@ impl Loan{
         }
     }
 }
+
 fn main(){
-    let l=Loan::new(30,Status::Pass);
+    let l= Loan::new(33,Status::Pass);
     match l.status{
         Status::Pass=>println!("pass {}",l.acc),
-        Status::Reje=>println!("reje {}",l.acc)
+        Status::reje=>println!("reje {}",l.acc),
     }
 }
