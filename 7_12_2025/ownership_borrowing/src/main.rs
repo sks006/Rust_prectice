5307dbb90b3a19f2241b95646397bfa50507a849
sks006 <mdshihabkabir07@gmail.com>
2025-12-07 21:55:42 +0600

    ownership _borrowing is done

---
+++ 7_12_2025/ownership_borrowing/src/main.rs
@@ -0,0 +1,258 @@
+// fn borrow(a:String)->String{
+//     println!("borrow {}",a);
+//     return a
+
+// }
+
+// fn main(){
+//     let l=String::from("kabir");
+//     let s=borrow(l);
+//     println!("this is {}",s)
+// }
+
+//--------------------------------- 19 -----------------------
+
+// fn borrow(a:String)->String{
+//     println!("borrow {}",a);
+//     return a
+// }
+// fn main(){
+//     let l=String::from("onto");
+//     let o=borrow(l);
+//     println!("lolo {}",o)
+// }
+
+//--------------------------------- 18 -----------------------
+// fn borrow(a:String)->String{
+//     println!("borrow {}",a);
+//     return a
+// }
+// fn main(){
+//     let l=String::from("shihab");
+//     let x=borrow(l);
+//     println!("borrow {}",x)
+// }
+
+//--------------------------------- 17 -----------------------
+
+// fn borrow (a:String)->String{
+//     println!("borrow {}",a);
+//     return a
+// }
+// fn main(){
+//     let l=String::from("fehim");
+//     let s=borrow(l);
+//     println!("borrow {}",s)
+// }
+//--------------------------------- 16 -----------------------
+// fn borrow (a:String)->u32{
+//     let x:u32=a.parse().expect("this is the end");
+//     let y:u32=122323;
+//     let z:u32=x-y;
+//     println!("this is the number {}",z);
+//     return z
+// }
+
+// fn main(){
+//     let l=String::from("10000000");
+//     let u=borrow(l);
+//     println!("main {}",u)
+// }
+//--------------------------------- 15 -----------------------
+
+// fn borrow(a:&String){
+//     let x:u32=a.parse().expect("this is expect 🦀🦀🦀 ");
+//     let y:u32=10000;
+//     let z:u32=x-y;
+//     println!("print z {}",z)
+// }
+
+// fn main(){
+//     let l=String::from("shihab");
+//     borrow(&l);
+//     println!("borrow {}",l)
+
+// }
+//--------------------------------- 14 -----------------------
+
+// fn borrow(a:String)->u32{
+//     let x:u32=a.parse().expect("this is the end");
+//     let y:u32=1000;
+//     let z:u32=x-y;
+//     // println!("borrow {}",z);
+//     return z
+// }
+
+// fn main (){
+//     let l=String::from("100000000");
+//     let k=borrow(l);
+//     println!("borrow {}",k)
+// }
+//--------------------------------- 13 -----------------------
+
+// fn borrow(a:String)->u32{
+//    let x:u32=a.parse().expect("this is the end");
+//    let y:u32=1200;
+//    let z:u32=x-y; 
+//    return z
+// }
+
+// fn main(){
+//     let l=String::from("30000");
+//     let d=borrow(l);
+//     println!("this is the number {}",d)
+// }
+//--------------------------------- 12 -----------------------
+
+// fn borrow(a:&String){
+//     let x:u32=a.parse().expect("this is the end");
+//     let y:u32=30303;
+//     let z:u32=x-y;
+
+// }
+
+// fn main(){
+//     let l=String::from("202020");
+//     borrow(&l);
+//     println!("borrow {}",l)
+// }
+//--------------------------------- 11 -----------------------
+
+// fn borrow(a:&String){
+//     let x:u32=a.parse().expect("this is the end");
+//     let y:u32=2000;
+//     let z:u32=x-y;
+    
+// }
+
+// fn main(){
+//     let l=String::from("44000");
+//     borrow(&l);
+//     println!("borrow {}",l)
+// }
+//--------------------------------- 10 -----------------------
+// fn borrow(a:String)->u32{
+//     let x:u32=a.parse().expect("this is the end");
+//     let y:u32=2000;
+//     let z:u32=x-y;
+//     return z
+// }
+
+// fn main(){
+//     let l=String::from("50000");
+//     let y=borrow(l);
+//     println!("borrow {}",y)
+// }
+//--------------------------------- 9 -----------------------
+
+// fn borrow(a:&String){
+//     let x:u32=a.parse().expect("this is the end");
+//     let y:u32=2000;
+//     let z:u32=x-y;
+
+// }
+
+// fn main(){
+//     let l=String::from("3000000");
+//     borrow(&l);
+//     println!("borrow {}",l)
+// }
+//--------------------------------- 8 -----------------------
+
+// fn borrow(a:String)->u32{
+//     let x:u32=a.parse().expect("this is the end");
+//     let y:u32=2000;
+//     let z:u32=x-y;
+//     return z
+// }
+
+// fn main(){
+//     let l=String::from("3000000");
+//     let d=borrow(l);
+//     println!("borrow {}",d)
+// }
+//--------------------------------- 7 -----------------------
+// fn borrow(a:String)->u32{
+//     let x:u32=a.parse().expect("this is the end");
+//     let y:u32=3000;
+//     let z:u32=x-y;
+//     return z
+// }
+// fn main(){
+//     let l=String::from("10000000");
+//     let x=borrow(l);
+//     println!("borrow {}",x)
+// }
+//--------------------------------- 6 -----------------------
+// fn borrow(a:String)->u32{
+//     let x:u32=a.parse().expect("this is the end");
+//     let y:u32=2000;
+//     let z:u32=x-y;
+//     return z
+// }
+// fn main(){
+//     let l=String::from("10000000");
+//     let x=borrow(l);
+//     println!("borrow {}",x)
+// }
+//--------------------------------- 5 -----------------------
+
+// fn borrow(a:String)->u32{
+//     let x:u32=a.parse().expect("this is the end");
+//     let y:u32=300000;
+//     let z:u32=x-y;
+//     return z
+// }
+
+// fn main(){
+//        let l=String::from("10000000");
+//     let x=borrow(l);
+//     println!("borrow {}",x) 
+// }
+//--------------------------------- 4 -----------------------
+// fn borrow(a:String)->String{
+//     println!("okok {}",a);
+//     return a
+// }
+// fn main(){
+//     let x=String::from("20000");
+//     let y=borrow(x);
+//     println!("this is the end {}",y);
+// }
+//--------------------------------- 3 -----------------------
+
+// fn borrow(a:String)->String{
+//     println!("okok {}",a);
+//     return a
+// }
+// fn main(){
+//     let x=String::from("200000");
+//     let y=borrow(x);
+//     println!("then its can me {}",y)
+// }
+//--------------------------------- 2 -----------------------
+// fn borrow(a:String)->u32{
+//     let x:u32=a.parse().expect("this is the end");
+//     let y:u32=1200;
+//     let z:u32=x-y;
+//     return z
+// }
+// fn main(){
+//     let x=String::from("200000");
+//     let y=borrow(x);
+//     println!("then its can me {}",y)
+// }
+
+//--------------------------------- 1 -----------------------
+
+fn borrow(a:&String){
+    let x:u32=a.parse().expect("this is the end");
+    let y:u32=1200;
+    let z:u32=x-y;
+
+}
+fn main(){
+    let x=String::from("200000");
+    borrow(&x);
+    println!("then its can me {}",x)
+}
